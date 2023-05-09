2da41982c768c8d30a68bf4c1a663f6e497c0cc0
AntonysamyChelladurai <68181796+AntonysamyChelladurai@users.noreply.github.com>
2023-05-09 08:21:53 +0530

    spring Kubernet all

--- src/main/java/com/jbean/currencyconversion/proxy/CurrencyExchangeProxy.java
+++ src/main/java/com/jbean/currencyconversion/proxy/CurrencyExchangeProxy.java
@@ -5,8 +5,8 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-//@FeignClient(name = "currency-exchange", url="http://localhost:8000")
-@FeignClient(name = "currency-exc-service")
+@FeignClient(name = "currency-exchange", url="http://currency-exchange:8100")
+//@FeignClient(name = "currency-exc-service")
 public interface CurrencyExchangeProxy {
     @GetMapping("/currency-exchange/from/{from}/to/{to}")
     public CurrencyConversion retrieveExchange(
